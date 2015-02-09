64596e6bd5631d4a2e275cb397c963b968f1f791
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-02-09 22:53:56 +0100

    Refactoring

--- package.js
+++ package.js
@@ -14,13 +14,20 @@ Package.onUse(function(api) {
   api.use('underscore');
 
   api.addFiles('lib/global.js', ['client', 'server']);
-  api.addFiles('lib/schema.js', ['client', 'server']);
+
+  // Schema.
+  api.addFiles('lib/schema/schema.js', ['client', 'server']);
+  api.addFiles('lib/schema/fields.js', ['client', 'server']);
+  api.addFiles('lib/schema/methods.js', ['client', 'server']);
+  api.addFiles('lib/schema/events.js', ['client', 'server']);
+  api.addFiles('lib/schema/validators.js', ['client', 'server']);
+  api.addFiles('lib/schema/behaviors.js', ['client', 'server']);
   api.addFiles('lib/model.js', ['client', 'server']);
-  api.addFiles('lib/behavior.js', ['client', 'server']);
   api.addFiles('lib/transform.js', ['client', 'server']);
   api.addFiles('lib/error.js', ['client', 'server']);
 
   // Behaviors.
+  api.addFiles('lib/behaviors/behavior.js', ['client', 'server']);
   api.addFiles('lib/behaviors/nestedset/node.js', ['client', 'server']);
   api.addFiles('lib/behaviors/nestedset/nestedset.js', ['client', 'server']);
   api.addFiles('lib/behaviors/sort/sort.js', ['client', 'server']);
@@ -31,11 +38,28 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
 
+  api.use('mongo');
+  api.use('minimongo');
+  api.use('underscore');
+
   api.addFiles('lib/global.js', ['client', 'server']);
-  api.addFiles('lib/schema.js', ['client', 'server']);
+
+  // Schema.
+  api.addFiles('lib/schema/schema.js', ['client', 'server']);
+  api.addFiles('lib/schema/fields.js', ['client', 'server']);
+  api.addFiles('lib/schema/methods.js', ['client', 'server']);
+  api.addFiles('lib/schema/events.js', ['client', 'server']);
+  api.addFiles('lib/schema/validators.js', ['client', 'server']);
+  api.addFiles('lib/schema/behaviors.js', ['client', 'server']);
   api.addFiles('lib/model.js', ['client', 'server']);
   api.addFiles('lib/transform.js', ['client', 'server']);
   api.addFiles('lib/error.js', ['client', 'server']);
+
+  // Behaviors.
+  api.addFiles('lib/behaviors/behavior.js', ['client', 'server']);
+  api.addFiles('lib/behaviors/nestedset/node.js', ['client', 'server']);
+  api.addFiles('lib/behaviors/nestedset/nestedset.js', ['client', 'server']);
+  api.addFiles('lib/behaviors/sort/sort.js', ['client', 'server']);
 
   api.addFiles('test/fields.js', ['client', 'server']);
 });
