36d90a301c088185ddff14cfcf9194779338a7f3
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-06-01 19:46:20 +0200

    Fix getting and setting non existing nested field

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Model layer for Meteor',
-  version: '0.10.1',
+  version: '0.10.2',
   name: 'jagi:astronomy',
   git: 'https://github.com/jagi/meteor-astronomy.git'
 });
