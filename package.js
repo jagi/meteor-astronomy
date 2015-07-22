0c7a7639e83e569bc3d1b565714ec5eb9ca48fe8
jagi <luke.jagodzinski@gmail.com>
2015-07-22 16:20:12 +0200

    Reorganize core modules

--- package.js
+++ package.js
@@ -14,6 +14,7 @@ Package.onUse(function(api) {
   api.use('ejson');
 
   api.addFiles('lib/core/global.js', ['client', 'server']);
+  api.addFiles('lib/core/drivers.js', ['client', 'server']);
   api.addFiles('lib/core/config.js', ['client', 'server']);
   api.addFiles('lib/core/utils.js', ['client', 'server']);
   api.addFiles('lib/core/events.js', ['client', 'server']);
@@ -27,32 +28,34 @@ Package.onUse(function(api) {
   // Events module.
   api.addFiles('lib/modules/events/init_module.js', ['client', 'server']);
   api.addFiles('lib/modules/events/init_class.js', ['client', 'server']);
-  api.addFiles('lib/modules/events/module.js', ['client', 'server']);
 
   // Types module.
-  api.addFiles('lib/modules/types/global.js', ['client', 'server']);
   api.addFiles('lib/modules/types/utils.js', ['client', 'server']);
-  api.addFiles('lib/modules/types/type.js', ['client', 'server']);
+  api.addFiles('lib/modules/types/type_definition.js', ['client', 'server']);
   api.addFiles('lib/modules/types/init_module.js', ['client', 'server']);
-  api.addFiles('lib/modules/types/module.js', ['client', 'server']);
 
   // Indexes module.
   api.addFiles('lib/modules/indexes/init_class.js', ['client', 'server']);
-  api.addFiles('lib/modules/indexes/module.js', ['client', 'server']);
 
   // Fields module.
   api.addFiles('lib/modules/fields/utils.js', ['client', 'server']);
+  api.addFiles('lib/modules/fields/field_definition.js', ['client', 'server']);
   api.addFiles('lib/modules/fields/init_module.js', ['client', 'server']);
   api.addFiles('lib/modules/fields/init_class.js', ['client', 'server']);
-  api.addFiles('lib/modules/fields/module.js', ['client', 'server']);
 
   // Methods module.
   api.addFiles('lib/modules/methods/init_class.js', ['client', 'server']);
-  api.addFiles('lib/modules/methods/module.js', ['client', 'server']);
 
   // EJSON module.
   api.addFiles('lib/modules/ejson/init_module.js', ['client', 'server']);
-  api.addFiles('lib/modules/ejson/module.js', ['client', 'server']);
+
+  // MongoDb driver.
+  api.addFiles([
+    'lib/drivers/mongo/init_driver.js',
+    'lib/drivers/mongo/field_definition.js',
+    'lib/drivers/mongo/type_definition.js',
+    'lib/drivers/mongo/types.js',
+  ], ['client', 'server']);
 
   api.export(['Astro', 'Astronomy'], ['client', 'server']);
 });
