0860ff14c01d6c3b27e09576acc28ac48ce8e236
jagi <luke.jagodzinski@gmail.com>
2015-08-24 18:29:20 +0200

    Trying inheritance

--- test/methods_definition.js
+++ test/methods_definition.js
@@ -1,8 +1,4 @@
 Tinytest.add('Methods - Definition', function(test) {
-  test.equal(_.size(Method.getMethods()), 2,
-    'The "Method" class should have 2 methods'
-  );
-
   var method = new Method({
     string: 'string',
     number: 123
