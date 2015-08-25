8903fe985d176fcc34e872c046d60a5188c983bf
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-08-25 22:54:11 +0200

    Indexes tests

--- test/indexes_init.js
+++ test/indexes_init.js
@@ -4,16 +4,57 @@ Tinytest.add('Indexes - Init', function(test) {
 
   Indexes = new Mongo.Collection('indexes');
 
+  try {
+    Indexes._dropIndex('object');
+    Indexes._dropIndex('array');
+    Indexes._dropIndex('field');
+    Indexes._dropIndex('indexes');
+  } catch (e) {}
+
   Index = Astro.Class({
     name: 'Index',
     collection: Indexes,
+    embedOne: {
+      'object': {
+        index: 1
+      }
+    },
+    embedMany: {
+      'array': {
+        index: 1
+      }
+    },
     fields: {
-      name: {
+      'field': {
         type: 'string',
         index: 1
+      },
+      'string': 'string',
+      'number': 'number'
+    },
+    indexes: {
+      'indexes': {
+        fields: {
+          string: 1,
+          number: 1
+        }
       }
     }
   });
 
-  console.log(1, Index.schema);
+  test.isTrue(Index.hasIndex('field'),
+    'Index defined in the field definition should be added'
+  );
+
+  test.isTrue(Index.hasIndex('object'),
+    'Index defined in the embeded (one) field definition should be added'
+  );
+
+  test.isTrue(Index.hasIndex('array'),
+    'Index defined in the embeded (many) field definition should be added'
+  );
+
+  test.isTrue(Index.hasIndex('indexes'),
+    'Index defined in the class definition should be added'
+  );
 });
