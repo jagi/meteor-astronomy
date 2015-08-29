ab4b76afcb28ce531f38ab29ef5b955b4d2e694e
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-08-29 19:11:56 +0200

    Add tests for inserting document with the _id field filled

--- test/storage_insert.js
+++ test/storage_insert.js
@@ -1,17 +1,7 @@
 Tinytest.add('Storage - Insert', function(test) {
-  var storage = new Storage();
-
-  storage.set('string', 'string');
-  storage.set('number', 123);
-  storage.set('boolean', true);
-  storage.set('date', new Date(2000, 0, 1, 0, 0, 0, 0));
-  storage.set('array', [1, 2, 3]);
-  storage.set('object', {
-    a: 'a',
-    b: 'b',
-    c: 'c'
-  });
-  storage.set('nested', new NestedCore({
+  var id = 'ekfAFb8w5umxaeAPs';
+  var storage = new Storage({
+    _id: id,
     string: 'string',
     number: 123,
     boolean: true,
@@ -21,15 +11,27 @@ Tinytest.add('Storage - Insert', function(test) {
       a: 'a',
       b: 'b',
       c: 'c'
-    }
-  }));
+    },
+    nested: new NestedCore({
+      string: 'string',
+      number: 123,
+      boolean: true,
+      date: new Date(2000, 0, 1, 0, 0, 0, 0),
+      array: [1, 2, 3],
+      object: {
+        a: 'a',
+        b: 'b',
+        c: 'c'
+      }
+    })
+  });
   storage.save();
 
-  storage = _.omit(Storages.findOne({}, {
+  storage = Storages.findOne({}, {
     transform: null
-  }), '_id');
-
+  });
   expectedCore = {
+    _id: id,
     string: 'string',
     number: 123,
     boolean: true,
@@ -53,7 +55,6 @@ Tinytest.add('Storage - Insert', function(test) {
       }
     }
   };
-
   test.equal(storage, expectedCore,
     'The document has not been saved properly'
   );
