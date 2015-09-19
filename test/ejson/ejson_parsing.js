063e2987b43ab971ed214fc4a231c7ba79c346b2
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-09-19 23:34:13 +0200

    Add test for EJSONification of nested fields

--- test/ejson/ejson_parsing.js
+++ test/ejson/ejson_parsing.js
@@ -2,14 +2,28 @@ Tinytest.add('EJSON - Parsing', function(test) {
   // Reset Astronomy.
   reset();
 
+  var NestedEJSON = Astro.Class({
+    name: 'NestedEJSON',
+    fields: {
+      'string': {
+        type: 'string'
+      },
+      'number': {
+        type: 'number'
+      }
+    }
+  });
+
   var EJSONClass = Astro.Class({
     name: 'EJSONClass',
     fields: {
       'object': {
-        type: 'object'
+        type: 'object',
+        nested: 'NestedEJSON'
       },
       'array': {
-        type: 'array'
+        type: 'array',
+        nested: 'NestedEJSON'
       },
       'string': {
         type: 'string'
@@ -31,15 +45,13 @@ Tinytest.add('EJSON - Parsing', function(test) {
     number: 123,
     boolean: true,
     object: {
-      a: 'a',
-      b: 'b',
-      c: 'c'
+      string: 'string',
+      number: 123
     },
-    array: [
-      1,
-      2,
-      3
-    ],
+    array: [{
+      string: 'string',
+      number: 123
+    }],
     date: new Date(2000, 0, 1, 0, 0, 0)
   });
 
@@ -55,11 +67,15 @@ Tinytest.add('EJSON - Parsing', function(test) {
   test.equal(typeof ejsonB.boolean, 'boolean',
     'The "boolean" field\'s value should be a boolean'
   );
-  test.instanceOf(ejsonB.object, Object,
-    'The "object" field\'s value should be an object'
+  test.instanceOf(ejsonB.object, NestedEJSON,
+    'The "object" field\'s value should be an instance of the NestedEJSON class'
   );
   test.instanceOf(ejsonB.array, Array,
     'The "array" field\'s value should be an array'
+  );
+  test.instanceOf(ejsonB.array[0], NestedEJSON,
+    'The "array" field\'s first value should be an instance of the ' +
+    'NestedEJSON class'
   );
   test.instanceOf(ejsonB.date, Date,
     'The "date" field\'s value should be a date'
