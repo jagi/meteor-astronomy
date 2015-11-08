e965ffd15fb46cf1c27193e265e3d517719b7343
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-11-08 13:11:21 +0100

    Update version to 1.2.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Model layer for Meteor',
-  version: '1.2.0',
+  version: '1.2.1',
   name: 'jagi:astronomy',
   git: 'https://github.com/jagi/meteor-astronomy.git'
 });
