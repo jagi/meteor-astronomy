7a144631148e905f266155826aa3c0c3102e499d
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2016-06-30 11:11:11 +0200

    Bump version to 2.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy',
-  version: '2.0.1',
+  version: '2.1.0',
   summary: 'Model layer for Meteor',
   git: 'https://github.com/jagi/meteor-astronomy.git'
 });
