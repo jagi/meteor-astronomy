df72f6ea7f199be236887fba023d4e048d074c62
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2017-06-05 08:40:48 +0200

    2.4.4 release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy',
-  version: '2.4.3',
+  version: '2.4.4',
   summary: 'Model layer for Meteor',
   git: 'https://github.com/jagi/meteor-astronomy.git'
 });
@@ -40,7 +40,7 @@ Package.onTest(function(api) {
     'insecure',
     'mongo',
     'ejson',
-    'jagi:astronomy@2.4.3'
+    'jagi:astronomy@2.4.4'
   ], ['client', 'server']);
 
   api.addFiles('test/utils.js', ['client', 'server']);
