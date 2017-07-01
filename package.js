1ea3640cc0749d83d8f125a3327c35c58d0eede7
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2017-07-01 22:26:40 +0200

    Bump version to 2.4.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy',
-  version: '2.4.6',
+  version: '2.4.7',
   summary: 'Model layer for Meteor',
   git: 'https://github.com/jagi/meteor-astronomy.git'
 });
@@ -40,7 +40,7 @@ Package.onTest(function(api) {
     'insecure',
     'mongo',
     'ejson',
-    'jagi:astronomy@2.4.6'
+    'jagi:astronomy@2.4.7'
   ], ['client', 'server']);
 
   api.addFiles('test/utils.js', ['client', 'server']);
