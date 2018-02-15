1a792f03d0ef1d300b7112bdb33d38a735d5ca66
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2018-02-15 21:47:40 +0100

    Bump version to 2.5.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy',
-  version: '2.5.3',
+  version: '2.5.4',
   summary: 'Model layer for Meteor',
   git: 'https://github.com/jagi/meteor-astronomy.git'
 });
@@ -37,7 +37,7 @@ Package.onTest(function(api) {
     'insecure',
     'mongo',
     'ejson',
-    'jagi:astronomy@2.5.3'
+    'jagi:astronomy@2.5.4'
   ], ['client', 'server']);
 
   api.addFiles('test/utils.js', ['client', 'server']);
