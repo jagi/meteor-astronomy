b238fbce73da94bba2fee75a8f55547c5b13a401
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2019-09-24 10:13:22 +0200

    Expose oldDoc in the server events

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "jagi:astronomy",
-  version: "2.7.2",
+  version: "2.7.3",
   summary: "Model layer for Meteor",
   git: "https://github.com/jagi/meteor-astronomy.git"
 });
